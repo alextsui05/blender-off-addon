2b0cc00468cd972828edc3cdd3b942307d0e6293
Alex Tsui <atsui@ucdavis.edu>
2014-01-22 12:04:07 -0800

    Complete basic implementation of OFF parse and load.

--- import_off.py
+++ import_off.py
@@ -1,3 +1,21 @@
+#####
+#
+# Copyright 2014 Alex Tsui
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+#####
+
 # 
 # http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Guidelines/Addons
 #
@@ -8,6 +26,8 @@ from bpy.props import (BoolProperty,
     StringProperty,
     )
 from bpy_extras.io_utils import (ImportHelper,
+    unpack_list,
+    unpack_face_list,
     )
 
 #if "bpy" in locals():
@@ -42,7 +62,15 @@ class ImportOFF(bpy.types.Operator, ImportHelper):
 
         keywords = self.as_keywords()
 
-        load(self, context, self.filepath)
+        mesh = load(self, context, self.filepath)
+        if not mesh:
+            return {'CANCELLED'}
+
+        scene = bpy.context.scene
+        obj = bpy.data.objects.new(mesh.name, mesh)
+        scene.objects.link(obj)
+        scene.objects.active = obj
+        obj.select = True
 
         return {'FINISHED'}
 
@@ -50,17 +78,16 @@ def menu_func_import(self, context):
     self.layout.operator(ImportOFF.bl_idname, text="OFF Mesh (.off)")
 
 def register():
-    print("Hello world")
     bpy.utils.register_module(__name__)
     bpy.types.INFO_MT_file_import.append(menu_func_import)
 
 def unregister():
-    print("Goodbye world")
     bpy.utils.unregister_module(__name__)
     bpy.types.INFO_MT_file_import.remove(menu_func_import)
 
 def load(operator, context, filepath):
-    print("TODO: load file")
+    # Parse mesh from OFF file
+    # TODO: Add support for NOFF and COFF
     filepath = os.fsencode(filepath)
     file = open(filepath, 'r')
     file.readline()
@@ -77,7 +104,19 @@ def load(operator, context, filepath):
         unused, vid1, vid2, vid3 = [int(x) for x in line.split()]
         facets.append((vid1, vid2, vid3))
 
-# TODO: How do i assemble a mesh object?
+    # Assemble mesh
+    off_name = bpy.path.display_name_from_filepath(filepath)
+    mesh = bpy.data.meshes.new(name=off_name)
+    mesh.vertices.add(len(verts))
+    mesh.vertices.foreach_set("co", unpack_list(verts))
+
+    mesh.tessfaces.add(len(facets))
+    mesh.tessfaces.foreach_set("vertices_raw", unpack_face_list(facets))
+
+    mesh.validate()
+    mesh.update()
+
+    return mesh
 
 if __name__ == "__main__":
     register()
